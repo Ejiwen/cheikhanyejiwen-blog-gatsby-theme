3cf08c407d3adfadb539ff733a83d805dc1f4749
Cheikhani <cheikhani@Mounas-Personal-MacBook-Pro.local>
2020-08-03 13:11:32 -0700

    test

--- gatsby-theme-ejiwen/src/components/Layout.js
+++ gatsby-theme-ejiwen/src/components/Layout.js
@@ -1,11 +1,23 @@
 import React from "react"
-import { Heading, Container } from "theme-ui"
+import { Heading, Container, Box, Flex } from "theme-ui"
 
 const Layout = ({ children }) => {
+  
   return (
     <div>
+      {/* <h2>hello</h2>
       <Heading>Gatsby Events Theme</Heading>
-      <Container>{children}</Container>
+      <Container>{children}</Container> */}
+
+<Flex>
+  <Box p={2} bg='muted'>
+  <Heading>Gatsby Events Theme</Heading>
+  </Box>
+  <Box p={2} bg='primary' sx={{ flex: '1 1 auto' }}>
+  <Container>{children}</Container>
+  </Box>
+</Flex>
+
     </div>
   )
 }
